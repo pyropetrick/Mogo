d415af70ccbd4103647ed2e83a7fa7036761b8b9
Pavel <pavel.chernenko97@gmail.com>
2022-07-23 12:19:58 +0300

    add home section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
+// const barLoader = document.querySelectorAll('.slider__bar-loader');
+// const barWhite = document.querySelector('.slider__bar-empty');
 
+
+
+
+// function draw(item) {
+  
+//   let pos = 0;
+//   let timer = setInterval(loading, 20);
+//   function loading() {
+//     if (pos === 100) {
+//       pos = 0;
+//       clearInterval(timer);
+//       item.style.width = pos + '%'
+//     } else {
+//       pos++
+//       item.style.width = pos + '%';
+//     }
+//   }
+// }
+
+// for (i of barLoader) {
+//   draw(i)
+// }
